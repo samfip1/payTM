0dc113074a350601f64eb2e8e2d632cf64fe98c3
samfip1 <samantrathod6@gmail.com>
2024-12-14 20:12:10 +0530

    Added Routers

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
-const app = express();
+const userRouter = require("./user")
 
 const router = express.Router();
+router.use('/user', userRouter)
 module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,6 @@ const express = require("express");
 const Rootrouter = require("./Routes/index")
 
 const app = express();
-app.use("/api/v1", Rootrouter)  //all request which are coming here go to Rootrouter.
+app.use("/api/v1", Rootrouter)  //all request which are coming here go to Rootrouter
+
+
