a1a98fe2bfa06179dacd1a6426b4f01e8df338fd
samfip1 <samantrathod6@gmail.com>
2024-12-14 16:11:07 +0530

    Added .env file

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
 
-mongoose.connect('mongodb+srv://samfip:bKIAxtd3OVqJ2lQt@samfip.gdqd29e.mongodb.net/payTM');
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
     firstname: String,
     lastname: String,
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
     User
-}
+};
