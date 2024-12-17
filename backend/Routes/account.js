eaedba713d5257f67b7b2f6d0d608a9b8be430b3
samfip1 <samantrathod6@gmail.com>
2024-12-17 22:41:29 +0530

    Changes in file

--- backend/Routes/account.js
+++ backend/Routes/account.js
@@ -3,3 +3,21 @@ const express = require("express")
 const router = express.Router();
 
 module.exports = {router};
+
+
+router.get('/balance', authMiddleware, async (req,res) => {
+    const acc = await User.Account.findOne({
+        userId: req.userId
+    })
+    
+    res.json({
+        balance: acc.balance
+    })
+
+})
+
+
+
+router.post('/transfer', authMiddleware, (req,res) => {
+    
+})
